ed6809588bb4ff1263d85ea948b3195bd4b87b10
Camilo Ardila <caardila41@gmail.com>
2020-10-15 17:31:57 -0500

    Agregé RegistrarCorreo

--- app/src/main/java/com/ard/agrocampo/MainActivity.java
+++ app/src/main/java/com/ard/agrocampo/MainActivity.java
@@ -2,7 +2,9 @@ package com.ard.agrocampo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void Registrate (View view ){
+        Intent registrate= new Intent(this,Registro.class);//Generar instancia entre pantallar
+        //Envio de info entre pantallas
+        startActivity(registrate);//Inicializar objeto
     }
 }
---
+++ app/src/main/java/com/ard/agrocampo/RegistrarCorreo.java
@@ -0,0 +1,14 @@
+package com.ard.agrocampo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrarCorreo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_correo);
+    }
+}
